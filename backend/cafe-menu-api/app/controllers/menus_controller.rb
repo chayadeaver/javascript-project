e50b4007bd94edd5ad25cb3b027ffb20a2552a5c
chayadeaver <chayadeaver@gmail.com>
2020-04-24 22:52:50 -0400

    added index and show actions

--- backend/cafe-menu-api/app/controllers/menus_controller.rb
+++ backend/cafe-menu-api/app/controllers/menus_controller.rb
@@ -1,2 +1,12 @@
 class MenusController < ApplicationController
+
+  def index
+    menus = Menu.all 
+    render json: MenuSerializer.new(menus)
+  end
+
+  def show
+    menu = Menu.find_by(id: params[:id])
+    render json: MenuSerializer.new(menu).serialized_json
+  end
 end
