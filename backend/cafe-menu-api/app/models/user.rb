67af275eb92b34d06157971c6902e812724e56e1
chayadeaver <chayadeaver@gmail.com>
2020-04-27 18:34:23 -0400

    set up devise

---
+++ backend/cafe-menu-api/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+          :jwt_authenticatable,
+          :registerable,
+          jwt_revocation_strategy: JwtBlacklist
+
+  has_many :orders
+  has_one :cart
+  
+end
--- backend/cafe-menu-api/config/routes.rb
+++ backend/cafe-menu-api/config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :menus
+  resources :carts do
+    resources :order_items
+  end
+  # devise_for :users
+  root to: "home#index"
+  # resources :order_items
+  resources :orders do
+    resources :order_items
+  end
+  # resources :items
+  resources :menus do
+    resources :items
+  end
+
+  resources :items do
+    resources :order_items
+  end
+
+  devise_for :users,
+            path: '',
+            path_names: {
+              sign_in: 'login',
+              sign_out: 'logout',
+              registration: 'signup'
+            },
+            controllers: {
+              sessions: 'sessions',
+              registrations: 'registrations'
+            }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
